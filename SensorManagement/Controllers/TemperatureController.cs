5ba18a39059422e9bb474869cd04cd45cf993f48
czuptab <bartekcz98@gmail.com>
2022-05-30 23:08:45 +0200

    add controllers

---
+++ SensorManagement/Controllers/TemperatureController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SensorManagement.Models.Temperature;
+using SensorManagement.Services;
+
+namespace SensorManagement.Controllers
+{
+    [ApiController]
+    [Route("[controller]/temperature")]
+    public class TemperatureController : ControllerBase
+    {
+        private ISensorService _temperatureService;
+        private IMapper _mapper;
+
+        public TemperatureController(
+            ISensorService temperatureService,
+            IMapper mapper)
+        {
+            _temperatureService = temperatureService;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult Create(TemperatureCreateRequest model)
+        {
+            _temperatureService.Create(model);
+            return Ok(new { message = "Temperature log created" });
+        }
+    }
+}
